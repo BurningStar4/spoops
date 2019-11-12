390ecaefe94430685eab8b357d77277f79036432
Lloyd <kevinbernardo24@gmail.com>
2019-11-13 00:16:55 +1100

    Added TurnOrder interface. No implementation yet

---
+++ Spoops/Game/Combat/ITurnBasedCombat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spoops.Game.Combat
+{
+   interface ITurnBasedCombat : ICombat
+    {
+        public void AssignTurnOrderMaster(ITurnOrderMaster newTurnOrderMaster);
+        public void RemoveTurnMaster();
+    }
+}
---
+++ Spoops/Game/Combat/ITurnOrderMaster.cs
@@ -0,0 +1,26 @@
+﻿using Spoops.Game.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spoops.Game.Combat
+{
+    public interface ITurnOrderMaster
+    {
+        public void AddActor(IActor newActor);
+
+        public void RemoveActor(IActor actor);
+
+        public void SendActorToFront(IActor actor);
+
+        public void SendActorToBack(IActor actor);
+
+        public void UpdateTurnOrder();
+
+        public void DelayTurnByAmount(IActor actor, int delayAmount);
+
+        public void ExecuteNextTurn();
+
+        public string GetFullTurnOrderString();
+    }
+}
