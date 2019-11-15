429831b98bd801b1477fd842a70eb826d7e79431
Lloyd <kevinbernardo24@gmail.com>
2019-11-15 23:52:14 +1100

    Stats update

--- Spoops/Game/Actor/Stats/ActorStat.cs
+++ Spoops/Game/Actor/Stats/ActorStat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Spoops.Game.Actor.Stats
@@ -7,15 +8,21 @@ namespace Spoops.Game.Actor.Stats
     public class ActorStat
     {
         private bool isDirty = true;
-        public double BaseValue { get; set; }
         private double statValue;
         private bool isForcedValue;
+        private double lastBaseValue;
+
+        public double BaseValue { get; set; }
+
+        public ReadOnlyCollection<ActorStatModifier> StatModifiers;
         public double StatValue { get
             {
-                if (isDirty && !isForcedValue)
+                if ((isDirty || lastBaseValue != BaseValue) && !isForcedValue)
                 {
                     {
+                        this.lastBaseValue = BaseValue;
                         this.statValue = CalculateFinalStatValue();
+                        isDirty = false;
                     }
                 }
                 return this.statValue;
@@ -28,16 +35,24 @@ namespace Spoops.Game.Actor.Stats
         {
             this.statValue = baseValue;
             statModifiers = new List<ActorStatModifier>();
+            StatModifiers = statModifiers.AsReadOnly();
         }
 
         public void AddActorStatModifier(ActorStatModifier newActorStatModifier)
         {
+            isDirty = true;
             statModifiers.Add(newActorStatModifier);
+            statModifiers.Sort(CompareModifierOrder);
         }
 
-        public void RemoveActorStatModifier(ActorStatModifier actorStatModifier)
+        public bool RemoveActorStatModifier(ActorStatModifier actorStatModifier)
         {
-            statModifiers.Remove(actorStatModifier);
+            if(statModifiers.Remove(actorStatModifier))
+            {
+                isDirty = true;
+                return true;
+            }
+            return false;
         }
 
         public void ForceValue(double newValue)
@@ -51,16 +66,66 @@ namespace Spoops.Game.Actor.Stats
             this.isForcedValue = false;
         }
 
+        public bool RemoveAllModifiersByID(string modifierID)
+        {
+            var didRemove = false;
+
+            for (int i = statModifiers.Count - 1; i>=0; i--)
+            {
+                if(statModifiers[i].ModifierID == modifierID)
+                {
+                    isDirty = true;
+                    didRemove = true;
+                    statModifiers.RemoveAt(i);
+                }
+            }
+
+            return didRemove;
+        }
+
         public virtual double CalculateFinalStatValue()
         {
             var finalValue = BaseValue;
+            var sumPercentAdditive = 0;
 
-            foreach(ActorStatModifier statMod in statModifiers)
+            for(int i = 0; i < statModifiers.Count; i++)
             {
-                
+                var statModifier = statModifiers[i];
+                if (statModifier.ModifierType == StatModType.PercentMultiplicative)
+                {
+                    finalValue *= 1 + statModifier.ModifierValue;
+                }
+                else if (statModifier.ModifierType == StatModType.PercentAdditive)
+                {
+                    if ( (i+1) >= statModifiers.Count || statModifiers[i + 1].ModifierType != StatModType.PercentAdditive)
+                    {
+                        finalValue *= 1 + sumPercentAdditive;
+                        sumPercentAdditive = 0;
+                    }
+                }
+                else
+                {
+                    finalValue += statModifier.ModifierValue;
+                }
             }
 
             return finalValue;
+        }
+
+        private int CompareModifierOrder(ActorStatModifier a, ActorStatModifier b)
+        {
+            if (a.ModifierOrder < b.ModifierOrder)
+            {
+                return -1;
+            }
+            else if (a.ModifierOrder > b.ModifierOrder)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
--- Spoops/Game/Actor/Stats/ActorStatModifier.cs
+++ Spoops/Game/Actor/Stats/ActorStatModifier.cs
@@ -4,13 +4,24 @@ using System.Text;
 
 namespace Spoops.Game.Actor.Stats
 {
+    public enum StatModType
+    {
+        Flat,
+        PercentAdditive,
+        PercentMultiplicative
+    }
     public class ActorStatModifier
     {
         public double ModifierValue { get; set; }
-        public string ModifierID { get; set; }
-        public ActorStatModifier(string modifierID, double newModifierAmount)
+        public string ModifierID { get; }
+        public StatModType ModifierType { get; }
+        public int ModifierOrder { get; }
+        public ActorStatModifier(string newModifierID, double newModifierAmount, StatModType newStatModifierType, int newStatModifierOrder)
         {
+            ModifierID = newModifierID;
             ModifierValue = newModifierAmount;
+            ModifierType = newStatModifierType;
+            ModifierOrder = newStatModifierOrder;
         }
     }
 }
